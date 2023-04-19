7ee1a92f3823836b9f33efb45ba01c1f824c0615
Muhamad Rausyan Fikri <9569417+mrfikri-ai@users.noreply.github.com>
2023-04-19 18:09:18 +0300

    Update cbf_multiomni_dist.m

--- collision_avoidance/cbf_multiomni_dist.m
+++ collision_avoidance/cbf_multiomni_dist.m
@@ -1,88 +1,73 @@
-% TODO: Finish the code
-% NOTE: This code is cbf with centralized safety control
-
 clc, clear, close all;
 
 % Define parameters
-Ts = 0.01; % in seconds (time sampling)
-Tmax = 8; % maximum simulation time
-time_hist = 0:Ts:Tmax;
+x0 = [-0.6 0.6 -0.2 0.4 0.2 0.5 0.6 0.6]';
+xg = [1.2 2.5 -0.4 2.5 0.4 2.5 -1.2 2.5]';
+Q = 2*eye(8);
+Ts = 0.01;
 
-robot_num = 4; % Number of robots (updated to 4)
-des_dist = 0.2; % Desired distance
+time = 1000;
+x = zeros(8,time);
+x(:,1) = x0;
+u = zeros(8,time);
 
-% Initialize array to store information
-n = length(time_hist);
-x_hist = zeros(n,2*robot_num);
-u_hist = zeros(n, 2 * robot_num);
-cbf = zeros(n,1); % Define cbf value
-gamma = 10; % Gamma value for CBF
+% Simulator  (Distributed)
+for k = 2:time
+    u_temp = [];
 
-% Initialize value [p_1x, p_1y, ...]'
-x_hist(1,:) = [-0.6, 0.6, -0.2, 0.4, 0.2, 0.5, 0.6, 0.6]; % Update to include initial positions for 4 robots
-
-% Initialize value [u_goal_x, u_goal_y ...]'
-x_goal = [1.2, 2.5, -0.4, 2.5, 0.4, 2.5, -1.2, 2.5]; % Update to include goal positions for 4 robots
-
-% Simulation loop
-for k = 1:n-1
-    for i = 1:robot_num
-        % Get array index to be sliced
-        idx = 2*i-1;
-
-        % Current state of the robot
-        x_i_k = x_hist(k, idx:idx+1);
-
-        % Compute control distributed
-        u_goal = (x_goal(idx:idx+1) - x_i_k); % Unit vector towards goal
-
-        % Compute control distributed
-        u_i = [1,1];
-
-        % Initialize CBF constraint values for 4 robots
-        hx_vals = norm(x_i_k(1) - x_i_k(2)).^2 - des_dist^2;
-        cbf(k) = gamma * hx_vals; 
-
-        % Compute the qudprog
-        Q = 2*eye(2);
-%         c = -2 * u_gtg((i-1)*2+1:i*2)';
-
-        % Update 
-        x_hist(k+1, idx:idx+1) = x_i_k + Ts*u_i;
+    for r = 1:4
+        Hi = [];
+        bi = [];
+        % Check if there are robots too near
+        for n = 1:4
+            if r ~= n
+                dist_2 = (x(2*r-1,k-1)- x(2*n-1,k-1))^2+(x(2*r,k-1)- x(2*n,k-1))^2;
+                if dist_2<=1
+                    Hi = [Hi;-2*(x(2*r-1,k-1)- x(2*n-1,k-1)) -2*(x(2*r,k-1)- x(2*n,k-1))];
+                    bi = [bi;10*(dist_2-0.2^2)^3];
+                end
+            end
+        end
+       Qi = 2*eye(2);
+       ci = -2*[xg(2*r-1)-x(2*r-1,k-1);xg(2*r)-x(2*r,k-1)];
+      
+        u_temp = [u_temp;quadprog(Qi,ci,Hi,bi,[],[],[],[],[],optimset('Display','off'))];
     end
+    u(:,k-1) = u_temp;
+    x(:,k) = x(:,k-1) + Ts*u(:,k-1);
 end
 
-for k=1:n-1
+for k=1:time-1
     clf; hold on; grid on;
     % Agent 1
-    plot(x_hist(:,1), x_hist(:,2), 'b.-', 'MarkerSize', 2); % plot the entire trajectory up to the current point
-    plot(x_hist(k,1), x_hist(k,2), 'ro', 'MarkerSize', 5); % plot the current point in red
+    plot(x(1,:), x(2,:), 'b.-', 'MarkerSize', 2); % plot the entire trajectory up to the current point
+    plot(x(1,k), x(2,k), 'bo', 'MarkerSize', 5); % plot the current point in red
 
     % Agent 2
-    plot(x_hist(:,3), x_hist(:,4), 'r.-', 'MarkerSize', 2); % plot the entire trajectory up to the current point
-    plot(x_hist(k,3), x_hist(k,4), 'ro', 'MarkerSize', 5); % plot the current point in red
+    plot(x(3,:), x(4,:), 'r.-', 'MarkerSize', 2); % plot the entire trajectory up to the current point
+    plot(x(3,k), x(4,k), 'ro', 'MarkerSize', 5); % plot the current point in red
 
     % Agent 3
-    plot(x_hist(:,5), x_hist(:,6), 'k.-', 'MarkerSize', 2); % plot the entire trajectory up to the current point
-    plot(x_hist(k,5), x_hist(k,6), 'ko', 'MarkerSize', 5); % plot the current point in red
+    plot(x(5,:), x(6,:), 'g.-', 'MarkerSize', 2); % plot the entire trajectory up to the current point
+    plot(x(5,k), x(6,k), 'go', 'MarkerSize', 5); % plot the current point in red
 
     % Agent 4
-    plot(x_hist(:,7), x_hist(:,8), 'k.-', 'MarkerSize', 2); % plot the entire trajectory up to the current point
-    plot(x_hist(k,7), x_hist(k,8), 'ko', 'MarkerSize', 5); % plot the current point in red
+    plot(x(7,:), x(8,:), 'k.-', 'MarkerSize', 2); % plot the entire trajectory up to the current point
+    plot(x(7,k), x(8,k), 'ko', 'MarkerSize', 5); % plot the current point in red
     
-    plot(x_goal(1),x_goal(2),'p','LineWidth',2,'MarkerSize',6,...
+    plot(xg(1),xg(2),'p','LineWidth',2,'MarkerSize',6,...
     'MarkerEdgeColor','k','MarkerFaceColor','k'); %Black Star for goal pos
-    plot(x_goal(3),x_goal(4),'p','LineWidth',2,'MarkerSize',6,...
+    plot(xg(3),xg(4),'p','LineWidth',2,'MarkerSize',6,...
     'MarkerEdgeColor','r','MarkerFaceColor','r'); %Red Star for goal pos
-    plot(x_goal(5),x_goal(6),'p','LineWidth',2,'MarkerSize',6,...
-    'MarkerEdgeColor','r','MarkerFaceColor','r'); %Red Star for goal pos
-    plot(x_goal(7),x_goal(8),'p','LineWidth',2,'MarkerSize',6,...
-    'MarkerEdgeColor','r','MarkerFaceColor','r'); %Red Star for goal pos
-    
-    viscircles([x_hist(k,1), x_hist(k,2)], 0.2); % plot the moving object
-    viscircles([x_hist(k,3), x_hist(k,4)], 0.2); % plot the moving object
-    viscircles([x_hist(k,5), x_hist(k,6)], 0.2); % plot the moving object
-    viscircles([x_hist(k,7), x_hist(k,8)], 0.2); % plot the moving object
+    plot(xg(5),xg(6),'p','LineWidth',2,'MarkerSize',6,...
+    'MarkerEdgeColor','b','MarkerFaceColor','b'); %Red Star for goal pos
+    plot(xg(7),xg(8),'p','LineWidth',2,'MarkerSize',6,...
+    'MarkerEdgeColor','g','MarkerFaceColor','g'); %Red Star for goal pos
+    hold off
+    viscircles([x(1,k), x(2,k)], 0.2); % plot the moving object
+    viscircles([x(3,k), x(4,k)], 0.2); % plot the moving object
+    viscircles([x(5,k), x(6,k)], 0.2); % plot the moving object
+    viscircles([x(7,k), x(8,k)], 0.2); % plot the moving object
     xlim([-2 2]); ylim([-2 5]);
     xlabel("pos x (m)"); ylabel("pos y (m)");
     title('Distributed Systems Control');
@@ -90,3 +75,48 @@ for k=1:n-1
     pause(0.01);
 end
 
+figure
+plot(1:time,sqrt(sum(abs(x(1:2,:)-x(3:4,:)).^2)), 'LineWidth',1.5)
+hold on
+plot(1:time,sqrt(sum(abs(x(1:2,:)-x(5:6,:)).^2)), 'LineWidth',1.5)
+plot(1:time,sqrt(sum(abs(x(1:2,:)-x(7:8,:)).^2)), 'LineWidth',1.5)
+plot(1:time,sqrt(sum(abs(x(3:4,:)-x(5:6,:)).^2)), 'LineWidth',1.5)
+plot(1:time,sqrt(sum(abs(x(3:4,:)-x(7:8,:)).^2)), 'LineWidth',1.5)
+plot(1:time,sqrt(sum(abs(x(5:6,:)-x(7:8,:)).^2)), 'LineWidth',1.5)
+ylim([0, 3]);
+title("Time series of euclidean distance between all pairs of robots")
+legend("(r1,r2)","(r1,r3)","(r1,r4)","(r2,r3)",...
+    "(r2,r4)","(r3,r4)",Location="best")
+xlabel("Time")
+ylabel("Distance")
+
+figure
+plot(1:time,xg(1)-x(1,:),'b-', 'LineWidth', 1.5, 'DisplayName', 'u_{gtg1}')
+hold on, grid on;
+plot(1:time,xg(3)-x(3,:), 'r-', 'LineWidth', 1.5, 'DisplayName', 'u_{gtg2}')
+plot(1:time,xg(5)-x(5,:), 'k-', 'LineWidth', 1.5, 'DisplayName', 'u_{gtg3}')
+plot(1:time,xg(7)-x(7,:), 'y-', 'LineWidth', 1.5, 'DisplayName', 'u_{gtg4}')
+plot(1:time,u(1,:), 'b--', 'LineWidth', 1.5, 'DisplayName', 'u_1')
+plot(1:time,u(3,:), 'r--', 'LineWidth', 1.5, 'DisplayName', 'u_2')
+plot(1:time,u(5,:), 'k--', 'LineWidth', 1.5, 'DisplayName', 'u_3')
+plot(1:time,u(7,:), 'y--', 'LineWidth', 1.5, 'DisplayName', 'u_4')
+
+legend({'$u_{gtg,1}$','$u_{gtg,2}$', '$u_{gtg,3}$','$$u_{gtg,4}$', '$u_1$','$u_2$', '$u_3$','$u_4$'},'Interpreter','latex')
+title("Nominal vs optimal control input in x direction")
+xlabel("Time")
+ylabel("Control input")
+
+figure
+plot(1:time,xg(2)-x(2,:),'b-', 'LineWidth', 1.5, 'DisplayName', 'u_{gtg1}')
+hold on, grid on;
+plot(1:time,xg(4)-x(4,:), 'r-', 'LineWidth', 1.5, 'DisplayName', 'u_{gtg2}')
+plot(1:time,xg(6)-x(6,:), 'k-', 'LineWidth', 1.5, 'DisplayName', 'u_{gtg3}')
+plot(1:time,xg(8)-x(8,:), 'y-', 'LineWidth', 1.5, 'DisplayName', 'u_{gtg4}')
+plot(1:time,u(2,:), 'b--', 'LineWidth', 1.5, 'DisplayName', 'u_1')
+plot(1:time,u(4,:), 'r--', 'LineWidth', 1.5, 'DisplayName', 'u_2')
+plot(1:time,u(6,:), 'k--', 'LineWidth', 1.5, 'DisplayName', 'u_3')
+plot(1:time,u(8,:), 'y--', 'LineWidth', 1.5, 'DisplayName', 'u_4')
+
+legend({'$u_{gtg,1}$','$u_{gtg,2}$', '$u_{gtg,3}$','$$u_{gtg,4}$', '$u_1$','$u_2$', '$u_3$','$u_4$'},'Interpreter','latex')
+title("Nominal vs optimal control input in y direction")
+xlabel("Time")
